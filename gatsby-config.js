ba89d08a0d325cf27dd7912ed97f314a05cd0844
vassilisyannoussis <vassilis24@gmail.com>
2021-06-10 23:33:43 +0200

    added gatsby-wordpress-source and gatsby-source-filesystem pluggins to config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,81 +1,116 @@
 require(`dotenv`).config({
-  path: `.env`,
+    path: `.env`,
 })
 
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
-  siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-  },
-  plugins: [
-    {
-      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
-      // See the theme's README for all available options
-      options: {
-        navigation: [
-          {
-            title: `Blog`,
-            slug: `/blog`,
-          },
-          {
-            title: `About`,
-            slug: `/about`,
-          },
-        ],
-        externalLinks: [
-          {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
-          },
-        ],
-      },
+    siteMetadata: {
+        siteTitleAlt: `Minimal Blog - Gatsby Theme`,
     },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: process.env.GOOGLE_ANALYTICS_ID,
-    //   },
-    // },
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#6B46C1`,
-        display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-gatsby-cloud`,
-    `gatsby-plugin-netlify`,
-    shouldAnalyseBundle && {
-      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
-      options: {
-        analyzerMode: `static`,
-        reportFilename: `_bundle.html`,
-        openAnalyzer: false,
-      },
-    },
-  ].filter(Boolean),
+    plugins: [
+        {
+            /**
+             * First up is the WordPress source plugin that connects Gatsby
+             * to your WordPress site.
+             *
+             * visit the plugin docs to learn more
+             * https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-source-wordpress/README.md
+             *
+             */
+            resolve: `gatsby-source-wordpress`,
+            options: {
+                // the only required plugin option for WordPress is the GraphQL url.
+                url: `http://localhost:10013/graphql`,
+            },
+        },
+        /**
+         * We need this plugin so that it adds the "File.publicURL" to our site
+         * It will allow us to access static url's for assets like PDF's
+         *
+         * See https://www.gatsbyjs.org/packages/gatsby-source-filesystem/ for more info
+         */
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `assets`,
+                path: `${__dirname}/content/assets`,
+            },
+        },
+        /**
+         * The following two plugins are required if you want to use Gatsby image
+         * See https://www.gatsbyjs.com/docs/gatsby-image/#setting-up-gatsby-image
+         * if you're curious about it.
+         */
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+            // See the theme's README for all available options
+            options: {
+                navigation: [
+                    {
+                        title: `Blog`,
+                        slug: `/blog`,
+                    },
+                    {
+                        title: `About`,
+                        slug: `/about`,
+                    },
+                ],
+                externalLinks: [
+                    {
+                        name: `Twitter`,
+                        url: `https://twitter.com/lekoarts_de`,
+                    },
+                    {
+                        name: `Instagram`,
+                        url: `https://www.instagram.com/lekoarts.de/`,
+                    },
+                ],
+            },
+        },
+        // {
+        //   resolve: `gatsby-plugin-google-analytics`,
+        //   options: {
+        //     trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        //   },
+        // },
+        `gatsby-plugin-sitemap`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+                short_name: `minimal-blog`,
+                description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+                start_url: `/`,
+                background_color: `#fff`,
+                theme_color: `#6B46C1`,
+                display: `standalone`,
+                icons: [
+                    {
+                        src: `/android-chrome-192x192.png`,
+                        sizes: `192x192`,
+                        type: `image/png`,
+                    },
+                    {
+                        src: `/android-chrome-512x512.png`,
+                        sizes: `512x512`,
+                        type: `image/png`,
+                    },
+                ],
+            },
+        },
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-gatsby-cloud`,
+        `gatsby-plugin-netlify`,
+        shouldAnalyseBundle && {
+            resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+            options: {
+                analyzerMode: `static`,
+                reportFilename: `_bundle.html`,
+                openAnalyzer: false,
+            },
+        },
+    ].filter(Boolean),
 }
